fa7664316ffdd6d508fe703d51b8e2f2b41d410f
mrpubg252-cmd <mrpubg252@example.com>
2026-06-15 21:42:31 +0300

    Fix: move index.html to root and clean up test scripts

--- test3.js
+++
@@ -1,7 +0,0 @@
-import http from "http";
-const req = http.get("http://127.0.0.1:3000/api/v1/episodes?url=" + encodeURIComponent("https://t.alooytv8.xyz/watch/cqc7rwwjoibp.html?key=9e5dac6e0584"), res => {
-  let data = "";
-  res.on("data", chunk => data += chunk);
-  res.on("end", () => console.log(JSON.parse(data).data.length));
-});
-req.end();
