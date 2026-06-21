8d2344f51daa50f17ec109f4d41c473179fe015e
mrpubg252-cmd <mrpubg252@example.com>
2026-06-21 13:16:47 +0300

    Clean wipe and upload latest custom updates

--- app/applet/test.js
+++
@@ -1,6 +0,0 @@
-import fetch from "node-fetch";
-
-fetch("https://series.albesriali03.workers.dev/?action=series&url=https://a.alooytv13.xyz/genre/turki/100.html")
-  .then(res => res.json())
-  .then(data => console.log(JSON.stringify(data, null, 2)))
-  .catch(err => console.error(err));
