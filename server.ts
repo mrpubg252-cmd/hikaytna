19985bc0543781f97a29b601ec11a681fcb5965b
mrpubg252-cmd <mrpubg252@example.com>
2026-07-06 22:52:42 +0300

    Clean wipe and upload latest custom updates

--- server.ts
+++ server.ts
@@ -3811,8 +3811,11 @@ document.head.appendChild(s);
     console.warn("Async non-blocking logo puller failed:", e);
   }
 
+
+  const PORT = process.env.PORT || 3000;
+
   app.listen(PORT, "0.0.0.0", () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port ${PORT}`);
   });
 }
 
